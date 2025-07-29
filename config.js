62bfe113124d222f5def56be27da93c6fa86a560
Luke Mourino <boaringpig>
2025-07-29 19:49:59 +0100

    the real version

---
+++ config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+	apiKey: "AIzaSyADzBwxIgkAO2IK4cg_Ch9_Gi0gwcpGBC0",
+	authDomain: "schinken-tracker.firebaseapp.com",
+	projectId: "schinken-tracker",
+	storageBucket: "schinken-tracker.firebasestorage.app",
+	messagingSenderId: "658749635035",
+	appId: "1:658749635035:web:6fa6c94d4d234ba39bcffe",
+};
